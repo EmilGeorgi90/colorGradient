56f2f193de2d7327208d4a5b7ff21971f00a5657
Emil Giorgi <eg@truelinked.com>
2019-04-10 10:04:50 +0200

    made better color picking and cropping and delete the file efter

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,26 +1,10 @@
 $(document).ready(function(){
     $.get("http://localhost:3000/image", function(data){
-        console.log(hexAToRGBA(data.color2));
-        $('#gradient').css({
-            'background-image':`linear-gradient(45deg, ${hexAToRGBA(data.color2)} 1%, ${hexAToRGBA(data.color1)} 100%), linear-gradient(-45deg, #494d71 0%, rgba(217, 230, 185, 0.5) 80%)`
-        });
+        console.log(data.color2);
+        $('#artistProfile').css({
+            'background-image':`linear-gradient(to left, ${data.color1}, ${data.color1})`
+        })
+        $(".bgCover .gradientOnImage").append(`<style>.gradientOnImage:after{ box-shadow: ${data.color1} 10vw 0px 95px -15px inset </style>`);
+
     })
 })
-function hexAToRGBA(h) {
-    let r = 0, g = 0, b = 0;
-
-    // 3 digits
-    if (h.length == 4) {
-      r = "0x" + h[1] + h[1];
-      g = "0x" + h[2] + h[2];
-      b = "0x" + h[3] + h[3];
-  
-    // 6 digits
-    } else if (h.length == 7) {
-      r = "0x" + h[1] + h[2];
-      g = "0x" + h[3] + h[4];
-      b = "0x" + h[5] + h[6];
-    }
-    
-    return "rgb("+ +r + "," + +g + "," + +b + ")";
-  }
--- Server/ColorThief.Server/ColorThief.Server/routes/index.js
+++ Server/ColorThief.Server/ColorThief.Server/routes/index.js
@@ -1,29 +1,37 @@
-﻿'use strict';
+﻿"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ColorThief = require('color-thief');
-const cloudinary = require('cloudinary');
-
+const ColorThief = require("color-thief");
+const cloudinary = require("cloudinary");
 
 /* GET home page. */
-cloudinary.config({ 
-    cloud_name: 'ddsuaqv5j', 
-    api_key: '883598229586869', 
-    api_secret: 'frpjAq-DTL9PLUoJDHoMtcLE2oM' 
-  });
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
-router.get('/image', function (req, res){
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    cloudinary.v2.uploader.upload("/home/my_image.jpg", 
-    function(error, result) {console.log(result, error)});
-            cloudinary.api.resource('y0zvhxnxfesbqi2rfarg',  
-                        function(result)  { 
-                            res.jsonp({color1: result.colors[0][0], color2: result.colors[1][0], color3: result.colors[2][0]}) 
-                        }, { colors: true });
-    })
-module.exports = router
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express" });
+});
+router.get("/image", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+    cloudinary.v2.uploader.upload(
+        "500_1550310935381_1550310935381.png", { width: 10, height: 300, crop: "fill", gravity: "south_west"}, 
+    function(error, result) {
+      console.log(result, error);
+
+  cloudinary.api.resource(
+    result.public_id,
+    function(result) {
+      res.jsonp({
+        color1: result.colors[0][0],
+      });
+    },
+    { colors: true }
+  );
+  cloudinary.v2.uploader.destroy(result.public_id);
+}
+);
+});
+module.exports = router;
